79bbf04e03af0e063449dbca40a31ba140d63660
Lisa Ha <lisamayha@gmail.com>
2014-08-27 09:43:45 -0700

    added error display for empty comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :set_product, only: [:create]
   def index
     @comments = Comment.all.order('created_at desc')
   end
 
   def create
-    @comment = Product.find(params[:product_id].to_i).comments.build(comment_params)
+    @comment = @product.comments.build(comment_params)
     if @comment.save
       redirect_to product_path(params[:product_id].to_i)
     else
       flash[:errors] = @comment.errors.full_messages
-      render template: "products/"+params[:product_id]
+      redirect_to product_path(@product)
     end
   end
 
@@ -17,8 +18,7 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:comment)
   end
-  def prod_id
-    params.require(:product_id).to_i
+  def set_product
+    @product = Product.find(params[:product_id])
   end
-
 end
